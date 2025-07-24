991e2f5d6040cd242789e2771d31cac8a4164d08
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-07-23 22:31:13 -0600

    feat: Se implementarons los selectores de motodo y se cambio la forma en que se arma la peticion de fetch

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,31 +5,74 @@ import viteLogo from '/vite.svg'
 export function Home() {
 
     const [url,setUrl] = useState('')
+    const [method, setMethod]=useState('GET')
     const [response,setResponse] = useState('')
 
     const handleSend = async()=>{
-        const res = await fetch(url);
-        const data = await res.json()
-        setResponse(data)
 
-        console.log(response)
+        const options = {
+            method: method, // viene de tu select: GET, POST, etc.
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        // Si es GET o DELETE, no envíes body
+        if (method !== 'GET' && method !== 'DELETE') {
+            options.body = JSON.stringify({ nombre: 'Ejemplo', valor: 123 });
+        }
+
+         try {
+            const res = await fetch(url, options);
+            const data = await res.json();
+            setResponse(data);
+            console.log(response)
+        } catch (err) {
+            console.error('Error en la petición:', err);
+        }
+        
     }
 
     return (
         <>
-            <div>
-                <img src={viteLogo} className="logo" alt="Vite logo" />
+            <div className='div-row'>
+                <div>
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </div>
+                <div>
+                    <h1>Lucy</h1>
+                </div>
             </div>
-            <h1>Lucy</h1>
-            <input className='text-field' type='text' placeholder='url' value={url} onChange={(event)=>setUrl(event.target.value)}/>
-            <button onClick={handleSend}>Send</button>
+
+            <div className='div-row'>
+                <div>
+                     <select className='select-method' id="metodo" value={method} onChange={(event)=>setMethod(event.target.value)}>
+                        <option value="GET">GET</option>
+                        <option value="POST">POST</option>
+                        <option value="PUT">PUT</option>
+                        <option value="PATCH">PATCH</option>
+                        <option value="DELETE">DELETE</option>
+                    </select>
+                </div>
+                <div className='div-col'>
+                    <input className='text-field' type='text' placeholder='url' value={url} onChange={(event)=>setUrl(event.target.value)}/>
+                </div>
+                <div className='div-col'>
+                    <button onClick={handleSend}>Send</button>
+                </div>
+            </div>
+            
+            <div className='div-response'>
+                <pre>{JSON.stringify(response, null, 2)}</pre>
+            </div>
+            
             <p>
                 Cliente http ligero
             </p>
             <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
+                Descubre mas sobre el proyecto aqui
             </p>
-            <pre>{JSON.stringify(response, null, 2)}</pre>
+            
         </>
     )
 }
