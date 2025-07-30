64895a596a0de16e461d46f66c0a2de9d96f40b4
Marco Antonio Guadalupe Rivas Guzman <za19012397@zapopan.tecmm.edu.mx>
2025-07-30 08:43:57 -0600

    feat: Se agrego el parametro del url, se corrigio la forma en que se aplicaba el .json

--- src/client/client.js
+++ src/client/client.js
@@ -1,5 +1,5 @@
 export class Client{
-    static async sendPeticion(method,body){
+    static async sendPeticion(method,body,url){
 
         let result = {}
 
@@ -19,14 +19,14 @@ export class Client{
             const res = await fetch(url, options)
             const end = performance.now()
             result.duration = (end-start)
-            const dataFetch = await res.json
-            const text = JSON.stringify(data)
+            const dataFetch = await res.json()
+            const text = JSON.stringify(dataFetch)
             result.size = new TextEncoder().encode(text).length
             result.data = dataFetch
             result.status = await res.status
 
             return result
-            
+
         }catch(error){
             console.log(error)
         }
