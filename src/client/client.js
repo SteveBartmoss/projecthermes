45640356658b08fcd947256ef3ec993abe4b7db3
Marco Antonio Guadalupe Rivas Guzman <za19012397@zapopan.tecmm.edu.mx>
2025-07-30 07:54:00 -0600

    feat: Se implemento una clase para manejar la peticion por https

---
+++ src/client/client.js
@@ -0,0 +1,34 @@
+export class Client{
+    static async sendPeticion(method,body){
+
+        let result = {}
+
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        }
+
+        if(method !=='GET' && method !=='DELETE'){
+            options.body = JSON.stringify(JSON.parse(body))
+        }
+
+        try{
+            const start = performance.now()
+            const res = await fetch(url, options)
+            const end = performance.now()
+            result.duration = (end-start)
+            const dataFetch = await res.json
+            const text = JSON.stringify(data)
+            result.size = new TextEncoder().encode(text).length
+            result.data = dataFetch
+            result.status = await res.status
+
+            return result
+            
+        }catch(error){
+            console.log(error)
+        }
+    }
+}
