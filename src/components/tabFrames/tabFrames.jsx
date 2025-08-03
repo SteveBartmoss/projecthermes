783c2b9cd9a7c9e8f642ba9a6b9b312590e6cf10
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-02 23:33:43 -0600

    feat: Se agrego un componente para manejar las tabs de la aplicacion

---
+++ src/components/tabFrames/tabFrames.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react"
+
+import './tabFrames.css'
+
+export function TabFrames({elements}){
+
+    const [currentTab,setCurrentTab] = useState(0)
+
+    const handleChangeTab=(element)=>{
+        setCurrentTab(element.id)
+    }
+
+    useEffect(() => {
+        if (elements.length > 0) {
+            setCurrentTab(elements[0].id)
+        }
+    }, [ ])
+
+    return(
+        <div>
+            <div className="container-head">
+                {
+                    elements.map(element => 
+                        <div key={element.id}>
+                            <p className={currentTab === element.id ? 'tab tab-actibe' : 'tab'} onClick={()=>handleChangeTab(element)}>{element.title}</p>
+                        </div>
+                    )
+                }
+            </div>
+            <div>
+                {
+                    elements.map(element =>
+                        <div key={element.id} className={currentTab === element.id ? '' : 'tab-close'}>
+                            {element.content}
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    )
+}
