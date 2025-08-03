29408755c132a0bbe7c3fa71ffb5719926914216
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-02 23:32:52 -0600

    feat: Se agrego contenido para la pagina principal de la aplicacion

--- src/pages/mainScreen.jsx
+++ src/pages/mainScreen.jsx
@@ -1,7 +1,27 @@
-import { DivCol, DivRow } from '../components/containers/containers'
+import { useState } from 'react'
+import { DivCol, DivGrow, DivRow } from '../components/containers/containers'
+import { TabFrames } from '../components/tabFrames/tabFrames'
 import viteLogo from '/vite.svg'
+import { HttpFrame } from '../frames/httpFrame'
+import { Btn } from '../components/btn/btn'
 
 export function MainScreen() {
+
+    const [count,setCount] = useState(1)
+    const [frames,setFrames] = useState([
+        {
+            id: count,
+            title: `http${count}`,
+            content: <HttpFrame />
+        }
+    ])
+
+    const handleAddTab=()=>{
+        const newCount = count+1
+        setCount(newCount)
+        setFrames([...frames,{id: newCount, title: `http${newCount}`, content: <HttpFrame /> }])
+    }
+
     return (
         <>
             <DivCol>
@@ -12,6 +32,22 @@ export function MainScreen() {
                     <div>
                         <h1 className='title'>Light client</h1>
                     </div>
+                    <div>
+                        <Btn title={'new tab'} handle={handleAddTab} />
+                    </div>
+                </DivRow>
+                <DivRow>
+                    <DivGrow>
+                        <TabFrames elements={frames} />
+                    </DivGrow>
+                </DivRow>
+                <DivRow>
+                    <p>
+                        Cliente http ligero
+                    </p>
+                    <p className="read-the-docs">
+                        Descubre mas sobre el proyecto aqui
+                    </p>
                 </DivRow>
             </DivCol>
         </>
