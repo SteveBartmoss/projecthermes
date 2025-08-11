f2d70c61594366136bf1d6b8e5fc7e38f592eaef
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-10 19:31:07 -0600

    feat: Se cambio la prop de los elements, por el uso del slice de los tabs, se implemento un boton de agregar dentro del mismo tabs para agregar otra tab, se implemento la funcion para agregar un elemento al slice de los tabs

--- src/components/tabFrames/tabFrames.jsx
+++ src/components/tabFrames/tabFrames.jsx
@@ -1,36 +1,50 @@
 import { useEffect, useState } from "react"
+import addImg from '/add.png'
 
 import './tabFrames.css'
 import { HttpFrame } from "../../frames/httpFrame"
+import { useDispatch, useSelector } from "react-redux"
+import { addTab } from "../../store/tabSlice"
 
 export function TabFrames({elements}){
 
-    const [currentTab,setCurrentTab] = useState(0)
+    const listFrames = useSelector(state => state.tabs)
+    const dispach = useDispatch()
+    const [currentTab,setCurrentTab] = useState(1)
 
     const handleChangeTab=(element)=>{
         setCurrentTab(element.id)
     }
 
+    const handleAddTab=()=>{
+        let counter = listFrames[listFrames.length-1].id
+        dispach(addTab({
+            id: counter+1,
+            title: `http${counter+1}`
+        }))
+    }
+
     useEffect(() => {
-        if (elements.length > 0) {
-            setCurrentTab(elements[0].id)
-        }
+        setCurrentTab(listFrames[0].id)
     }, [ ])
 
     return(
         <div>
             <div className="container-head">
                 {
-                    elements.map(element => 
+                    listFrames.map(element => 
                         <div key={element.id}>
                             <p className={currentTab === element.id ? 'tab tab-actibe' : 'tab'} onClick={()=>handleChangeTab(element)}>{element.title}</p>
                         </div>
                     )
                 }
+                <div>
+                    <img onClick={handleAddTab} className="tab" src={addImg} alt="add" />
+                </div>
             </div>
             <div>
                 {
-                    elements.map(element =>
+                    listFrames.map(element =>
                         <div key={element.id} className={currentTab === element.id ? '' : 'tab-close'}>
                             <HttpFrame />
                         </div>
