0b56165dfb7c1cf8776afb5eafafbbbde275fd66
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-14 22:12:28 -0600

    feat: Se configuro la seccion para configurar un token en su propio apartado

--- src/components/authConfig/authConfig.jsx
+++ src/components/authConfig/authConfig.jsx
@@ -1,16 +1,20 @@
+import { useFrameContext } from '../../context/frameContext'
 import './authConfig.css'
 
-export function AuthConfig({type,setType,token,handleToken}){
+export function AuthConfig(){
+
+    const [typeToken,setTypeToken] = useFrameContext()
+    const [token,setToken] = useFrameContext()
 
     return(
         <div>
             <div>
-                 <select>
+                 <select className='select-method' value={typeToken} onChange={(event)=>setTypeToken(event.target.value)}>
                     <option>Bearer Token</option>
                 </select>
             </div>
            <div>
-                <input type="text" placeholder='Token' value={token} onChange={handleToken} />
+                <input type="text" className='text-field' placeholder='Token' value={token} onChange={(event)=>setToken(event.target.value)} />
            </div>
         </div>
     )
