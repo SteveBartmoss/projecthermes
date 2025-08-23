23b712fae518a2735ea47cd6726a81da240f280e
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-23 11:20:05 -0600

    feat: Se implemento el componente para los parametros

--- src/components/params/params.jsx
+++ src/components/params/params.jsx
@@ -1,9 +1,14 @@
+import { useFrameContext } from "../../context/frameContext";
+import './params.css'
 
 
 export function Params(){
+
+    const [params,setParams] = useFrameContext()
+
  return(
     <div>
-        <h1>Params</h1>
+        <textarea name="text-area" placeholder="Params" value={params} onChange={(event)=>setParams(event.target.value)} ></textarea>
     </div>
  )   
 }
