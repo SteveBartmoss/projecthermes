508fe790fa272a519cebe8b8cfba0b7cd9d00113
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-23 10:55:47 -0600

    feat: Se implemento el  nuevo componente para la configuracion del body

---
+++ src/components/body/bodyConfig.jsx
@@ -0,0 +1,18 @@
+import { useFrameContext } from "../../context/frameContext";
+import './bodyConfig.css'
+
+export function BodyConfig(){
+
+    const [typeBody,setTypeBody] = useFrameContext()
+    
+    return(
+        <div>
+            <div>
+                <select className='select-method' value={typeBody} onChange={(event)=>setTypeBody(event.target.value)}>
+                    <option value="body" >Body</option>
+                    <option value="formdata">Form-Data</option>
+                </select>
+            </div>
+        </div>
+    )
+}
