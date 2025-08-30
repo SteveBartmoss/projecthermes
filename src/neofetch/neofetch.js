58bfd79b6bd00ce9bc9452c10ace08ba6a272b3e
Marco <za19012397@zapopan.tecmm.edu.mx>
2025-08-30 14:50:25 -0600

    feat: Se inicializa el grapper para fetch

---
+++ src/neofetch/neofetch.js
@@ -0,0 +1,26 @@
+
+
+export class Neofetch{
+
+
+    static async get(){
+
+    }
+
+    static async post(){
+
+    }
+
+    static async put(){
+
+    }
+
+    static async patch(){
+
+    }
+
+    static async delete(){
+        
+    }
+
+}
